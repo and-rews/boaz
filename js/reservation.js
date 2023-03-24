c6a41319249a0d1ba3fb711d5d9834defc9346f0
and-rews <oseiandrews394@gmail.com>
2023-03-23 12:05:02 -1200

    user-details

--- js/reservation.js
+++ js/reservation.js
@@ -1,47 +1,37 @@
-// // const form = document.getElementById("reservation-form");
-
-// // form.addEventListener("submit", (event) => {
-// //   event.preventDefault(); // prevent the form from submitting normally
-
-// //   const formData = new FormData(form); // create a new FormData object to store the form data
-
-// //   fetch("https://boaz.hotelipad.com/api/sr", {
-// //     method: "POST",
-// //     body: formData,
-// //   })
-// //     .then((response) => {
-// //       if (!response.ok) {
-// //         throw new Error("Network response was not ok");
-// //       }
-// //       return response.json();
-// //     })
-// //     .then((data) => {
-// //       console.log(data); // handle the response data
-// //     })
-// //     .catch((error) => {
-// //       console.error("There was a problem with the fetch operation:", error);
-// //     });
-// // });
-
 // $(document).ready(function () {
-//   $("#reservation-form").submit(function (event) {
-//     // Prevent the default form submission behavior
-//     event.preventDefault();
-
-//     // Serialize the form data into a URL-encoded string
-//     var formData = $(this).serialize();
+//   $("#confirm-booking").click(function (event) {
+//     var name = document.getElementById("name").value;
+//     var phone_number = document.getElementById("phone-number").value;
+//     var arrival_date = document.getElementById("check-in").value;
+//     var departure_date = document.getElementById("check-out").value;
+//     var room_type = document.getElementById("room-type").value;
+//     var number_of_guests = document.getElementById("number-of-guests").value;
+//     var number_of_children =
+//       document.getElementById("number-of-children").value;
+//     var number_of_adults = document.getElementById("number-of-adults").value;
+//     var total_price = document.getElementById("total-price").value;
 
 //     // Send the AJAX request
 //     $.ajax({
 //       url: "https://boaz.hotelipad.com/api/sr",
 //       method: "POST",
-//       data: formData,
+//       data: {
+//         name: name,
+//         phone_number: phone_number,
+//         arrival_date: arrival_date,
+//         departure_date: departure_date,
+//         room_type: room_type,
+//         number_of_guests: number_of_guests,
+//         number_of_children: number_of_children,
+//         number_of_adults: number_of_adults,
+//         total_price: total_price,
+//       },
 //       dataType: "json",
 //       success: function (data) {
 //         // Handle successful response here
 //         alert("Your reservation was successfully booked!");
-//         $("#reservation-form")[0].reset(); // Reset the form
-//         window.location.href = "./booking-details.html"; // Redirect to booking details page
+//         $("#form-details")[0].reset(); // Reset the form
+//         window.location.href = "./"; // Redirect to booking details page
 //       },
 //       error: function (error) {
 //         // Handle error here
